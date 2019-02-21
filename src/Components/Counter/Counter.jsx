419040f6395b81efce8b4d007d8cb6ec3f212a94
Timothy Seah <timothyseah.t.e@gmail.com>
2019-02-21 11:19:14 +0800

    Create Part 2 Counter

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -3,11 +3,36 @@ import React, { Component } from "react";
 export class Counter extends Component {
   state = { id: 1, value: 0 };
 
+  handlePlus = () => {
+    const increment = this.state.value + 1;
+    this.setState({ value: increment });
+  };
+  handleMinus = () => {
+    const decrement = this.state.value - 1;
+    this.setState({ value: decrement });
+  };
+
   render() {
     return (
-      <h2>
-        <span class="badge badge-secondary">{this.state.value}</span>
-      </h2>
+      <React.Fragment>
+        <h2>
+          <span className="badge badge-secondary">{this.state.value}</span>
+        </h2>
+        <button
+          type="button"
+          onClick={this.handlePlus}
+          className="btn btn-success"
+        >
+          +
+        </button>
+        <button
+          type="button"
+          onClick={this.handleMinus}
+          className="btn btn-danger"
+        >
+          -
+        </button>
+      </React.Fragment>
     );
   }
 }
